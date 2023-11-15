74913de63f2c30f6589d6e7347f19b692ff083f6
Erik Sundell <erik.sundell87@gmail.com>
2023-11-15 17:26:55 +0100

    remove some tests

--- tests/datasource-plugin/queryEditor.spec.ts
+++ tests/datasource-plugin/queryEditor.spec.ts
@@ -1,11 +1,6 @@
 import { expect, test } from '../../src';
 import { ds } from './datasource';
-import {
-  QUERY_DATA_ERROR_RESPONSE,
-  QUERY_DATA_QUERY_A_RESPONSE,
-  QUERY_DATA_TIMESERIES_RESPONSE,
-} from './mocks/queryDataResponse';
-import { TABLE_RESPONSE } from './mocks/resourceResponse';
+import { QUERY_DATA_ERROR_RESPONSE } from './mocks/queryDataResponse';
 
 test('autocompletion engine should load tables', async ({ panelEditPage, page }) => {
   await panelEditPage.datasource.set(ds.name!);
@@ -31,41 +26,41 @@ test('should load schemas and display them on screen when clicking on schema dro
   await expect(page.getByText('average_temperature')).toBeTruthy();
 });
 
-test('fill in new query, run it and assert on result in table panel', async ({ panelEditPage, page }) => {
-  await panelEditPage.mockQueryDataResponse(QUERY_DATA_QUERY_A_RESPONSE);
-  await panelEditPage.mockResourceResponse('tables', TABLE_RESPONSE);
-  await panelEditPage.setVisualization('Table');
-  await panelEditPage.datasource.set(ds.name!);
-  await panelEditPage.timeRange.set({ from: '2021-01-01', to: '2021-01-02' });
-  const queryEditorRow = await panelEditPage.getQueryEditorRow('A');
+// test('fill in new query, run it and assert on result in table panel', async ({ panelEditPage, page }) => {
+//   await panelEditPage.mockQueryDataResponse(QUERY_DATA_QUERY_A_RESPONSE);
+//   await panelEditPage.mockResourceResponse('tables', TABLE_RESPONSE);
+//   await panelEditPage.setVisualization('Table');
+//   await panelEditPage.datasource.set(ds.name!);
+//   await panelEditPage.timeRange.set({ from: '2021-01-01', to: '2021-01-02' });
+//   const queryEditorRow = await panelEditPage.getQueryEditorRow('A');
 
-  // ds specific tests
-  await panelEditPage.getByTestIdOrAriaLabel('data-testid table', queryEditorRow).locator('input').click();
-  await page.getByText('average_temperature').last().click();
-  await panelEditPage.getCodeEditor(queryEditorRow).then((l) => l.click());
-  await page.keyboard.insertText('SELECT eventname FROM event ORDER BY eventname ASC LIMIT 1');
+//   // ds specific tests
+//   await panelEditPage.getByTestIdOrAriaLabel('data-testid table', queryEditorRow).locator('input').click();
+//   await page.getByText('average_temperature').last().click();
+//   await panelEditPage.getCodeEditor(queryEditorRow).then((l) => l.click());
+//   await page.keyboard.insertText('SELECT eventname FROM event ORDER BY eventname ASC LIMIT 1');
 
-  await panelEditPage.refreshPanel();
-  await panelEditPage.tablePanel.expectToContainText('RESULT 1');
-});
+//   await panelEditPage.refreshPanel();
+//   await panelEditPage.tablePanel.expectToContainText('RESULT 1');
+// });
 
-test('fill in new query, run it and assert on result in timeseries panel', async ({ panelEditPage, page }) => {
-  await panelEditPage.mockQueryDataResponse(QUERY_DATA_TIMESERIES_RESPONSE);
-  await panelEditPage.mockResourceResponse('tables', TABLE_RESPONSE);
-  await panelEditPage.setVisualization('Time series');
-  await panelEditPage.datasource.set(ds.name!);
-  await panelEditPage.timeRange.set({ from: '2008-01-01', to: '2008-01-03' });
-  const queryEditorRow = await panelEditPage.getQueryEditorRow('A');
+// test('fill in new query, run it and assert on result in timeseries panel', async ({ panelEditPage, page }) => {
+//   await panelEditPage.mockQueryDataResponse(QUERY_DATA_TIMESERIES_RESPONSE);
+//   await panelEditPage.mockResourceResponse('tables', TABLE_RESPONSE);
+//   await panelEditPage.setVisualization('Time series');
+//   await panelEditPage.datasource.set(ds.name!);
+//   await panelEditPage.timeRange.set({ from: '2008-01-01', to: '2008-01-03' });
+//   const queryEditorRow = await panelEditPage.getQueryEditorRow('A');
 
-  // ds specific tests
-  await panelEditPage.getByTestIdOrAriaLabel('data-testid table', queryEditorRow).locator('input').click();
-  await page.getByText('average_temperature').last().click();
-  await panelEditPage.getCodeEditor(queryEditorRow).then((l) => l.click());
-  await page.keyboard.insertText('SELECT starttime,eventid,dateid FROM event ORDER BY starttime ASC LIMIT 100');
+//   // ds specific tests
+//   await panelEditPage.getByTestIdOrAriaLabel('data-testid table', queryEditorRow).locator('input').click();
+//   await page.getByText('average_temperature').last().click();
+//   await panelEditPage.getCodeEditor(queryEditorRow).then((l) => l.click());
+//   await page.keyboard.insertText('SELECT starttime,eventid,dateid FROM event ORDER BY starttime ASC LIMIT 100');
 
-  await panelEditPage.refreshPanel();
-  await panelEditPage.timeSeriesPanel.expectToContainLegendLabels(['eventid', 'dateid']);
-});
+//   await panelEditPage.refreshPanel();
+//   await panelEditPage.timeSeriesPanel.expectToContainLegendLabels(['eventid', 'dateid']);
+// });
 
 test('fill in bad query, run it and assert that panel has error', async ({ panelEditPage, page }) => {
   await panelEditPage.datasource.set(ds.name!);
